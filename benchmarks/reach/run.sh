160d29d22592236e27eb3b9db1d31bed788f10d5
Ubuntu <ubuntu@ip-172-31-14-56.ec2.internal>
2026-02-08 18:32:33 +0000

    .

---
+++ benchmarks/reach/run.sh
@@ -0,0 +1,10 @@
+echo "wiki"
+time ~/souffle/bin/bin/souffle reach-wiki.dl -F/home/ubuntu/datasets/snap/
+echo "facebook"
+time ~/souffle/bin/bin/souffle reach-facebook.dl -F/home/ubuntu/datasets/snap/
+echo "ca"
+time ~/souffle/bin/bin/souffle reach-ca.dl -F/home/ubuntu/datasets/snap/
+echo "soc"
+time ~/souffle/bin/bin/souffle reach-soc.dl -F/home/ubuntu/datasets/snap/
+echo "twitter"
+time ~/souffle/bin/bin/souffle reach-twitter.dl -F/home/ubuntu/datasets/snap/
